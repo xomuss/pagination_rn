a32bb9eab5da59951b41e54fee47413931bfe75f
xomuss <olexandr.khomenko88@gmail.com>
2022-01-15 22:21:39 +0200

    pagination

--- App.js
+++ App.js
@@ -1,111 +1,110 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  ActivityIndicator,
+  FlatList,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
+  StyleSheet,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [dataSource, setDataSource] = useState([]);
+  const [offset, setOffset] = useState(1);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  useEffect(() => getData(), []);
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const getData = () => {
+    console.log('get data');
+    setLoading(true);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    // Your API key is: 69a6a8427b1c4a16b36457c02d33290d
+    //Service to get the data from the server to render
+    fetch(
+      `https://newsapi.org/v2/everything?q=bitcoin&pageSize=10&page=${offset}&apiKey=69a6a8427b1c4a16b36457c02d33290d`,
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        setOffset(offset + 1);
+        setDataSource([...dataSource, ...responseJson.articles]);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const renderFooter = () => {
+    return (
+      <View>
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={getData}
+          style={styles.loadMoreBtn}>
+          <Text style={styles.btnText}>Load More</Text>
+          {loading ? (
+            <ActivityIndicator color="white" style={{marginLeft: 8}} />
+          ) : null}
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const itemView = ({item}) => {
+    return <Text style={styles.itemStyles}>{item.title}</Text>;
+  };
+
+  const itemSeparatorView = () => {
+    return (
+      <View style={{height: 0.5, width: '100%', backgroundColor: '#C8C8C8'}} />
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <FlatList
+          data={dataSource}
+          renderItem={itemView}
+          ListFooterComponent={renderFooter}
+          ItemSeparatorComponent={itemSeparatorView}
+          enableEmptySections={true}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  itemStyles: {
+    paddingVertical: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  loadMoreBtn: {
+    padding: 10,
+    backgroundColor: '#800000',
+    borderRadius: 4,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+    paddingHorizontal: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  footer: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 15,
+    textAlign: 'center',
   },
 });
 
